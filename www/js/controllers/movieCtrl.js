172fb4f4161791932bb72419a85ed62471830bc0
Vincent Maubert <v.maubert@code-troopers.com>
2016-11-06 15:03:40 +0100

    Second session : can consult movie details

--- www/js/controllers/movieCtrl.js
+++ www/js/controllers/movieCtrl.js
@@ -1,5 +1,15 @@
 angular.module('controllers')
 
-  .controller('MovieCtrl', function ($scope, $stateParams) {
+  .controller('MovieCtrl', function ($scope, $stateParams, $http) {
 
+    $scope.movie = {title: "Chargement..."};
+    $http.jsonp('http://api-beta.rottentomatoes.com/api/public/v1.0/movies/'+$stateParams.movieId+'.json', {
+        params: {
+          apikey: '7waqfqbprs7pajbz28mqf6vz',
+          callback: 'JSON_CALLBACK'
+        }
+      })
+      .success(function (data) {
+        $scope.movie = data;
+      });
   });
