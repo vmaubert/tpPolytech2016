7cc9eaba525e7f9323c0da7765617c6f9d02435b
Vincent Maubert <v.maubert@code-troopers.com>
2016-11-06 19:11:37 +0100

    Fourth session : Add a google map

--- www/js/controllers/movieCtrl.js
+++ www/js/controllers/movieCtrl.js
@@ -1,6 +1,6 @@
 angular.module('controllers')
 
-  .controller('MovieCtrl', function ($scope, $stateParams, $http, rottenTomatoesConst) {
+  .controller('MovieCtrl', function ($scope, $stateParams, $http, rottenTomatoesConst, $cordovaGeolocation) {
 
     $scope.movie = {title: "Chargement..."};
     $http.jsonp(rottenTomatoesConst.url+'movies/'+$stateParams.movieId+'.json', {
@@ -11,5 +11,36 @@ angular.module('controllers')
       })
       .success(function (data) {
         $scope.movie = data;
+      });
+
+    $scope.options = {scrollwheel: false, mapTypeId: "roadmap" };
+    var icon = {
+      url: "img/logo-mega-cgr.png",
+      scaledSize: new google.maps.Size(50, 50)
+    };
+    $scope.markers = [];
+
+    // get position of user and then set the center of the map to that position
+    $cordovaGeolocation
+      .getCurrentPosition()
+      .then(function (position) {
+        var lat  = position.coords.latitude;
+        var long = position.coords.longitude;
+        $scope.map = {center: {latitude: lat, longitude: long}, zoom: 14 };
+
+          $scope.markers.push({
+            id: $scope.markers.length,
+            latitude: 47.3650755,
+            longitude: 0.685654,
+            icon: icon,
+            content: "Méga CGR"
+          });
+
+        $scope.onMarkerClick = function(marker, eventName, model) {
+          model.show = !model.show;
+        }
+
+      }, function(err) {
+        console.log(err);
       });
   });
