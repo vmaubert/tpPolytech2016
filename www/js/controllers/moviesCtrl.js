d68dd10a7f3fe80e6b78903dff26ea717cd0e568
Vincent Maubert <v.maubert@code-troopers.com>
2016-11-06 15:16:40 +0100

    Third session : add infinite scroll

--- www/js/controllers/moviesCtrl.js
+++ www/js/controllers/moviesCtrl.js
@@ -2,15 +2,28 @@ angular.module('controllers')
 
   .controller('MoviesCtrl', function ($scope, $http, rottenTomatoesConst) {
     $scope.movies = [];
-    $http.jsonp(rottenTomatoesConst.url + 'lists/movies/box_office.json', {
-        params: {
-          limit: 16,
-          country: 'us',
-          apikey: rottenTomatoesConst.apiKey,
-          callback: 'JSON_CALLBACK'
-        }
-      })
-      .success(function (data) {
-        $scope.movies = data.movies;
-      });
+    var page = 1;
+    var maxResult = 1;
+
+    $scope.loadMore = function () {
+      $http.jsonp(rottenTomatoesConst.url + 'lists/movies/in_theaters.json', {
+          params: {
+            page_limit: 16,
+            page: page,
+            country: 'us',
+            apikey: rottenTomatoesConst.apiKey,
+            callback: 'JSON_CALLBACK'
+          }
+        })
+        .success(function (data) {
+          $scope.movies = $scope.movies.concat(data.movies);
+          maxResult = data.total;
+          page++;
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    };
+
+    $scope.isNotFullLoaded = function(){
+      return $scope.movies.length < maxResult;
+    }
   });
